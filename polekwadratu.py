8d79c7060be288aa32d222542b72228dad35076a
Hacinson <myboyc30@gmail.com>
2024-06-30 09:15:50 +0200

    dodanie dwóch plików do osobnego branchu

---
+++ polekwadratu.py
@@ -0,0 +1,7 @@
+#Napisz program,
+#ktory wyswietla wartosc pola kwadratu o podanym przez uzytkownika boku.
+
+
+bok = int(input("Podaj długość boku kwadratu: "))
+pole = bok * bok
+print("Pole kwadratu o boku", bok, "wynosi", pole)
